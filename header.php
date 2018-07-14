22f670a801a30800057c51222c8f18b2ea460a96
Bernard Huff <bernard.huff008@gmail.com>
2018-07-13 23:22:23 -0500

    Added a logo to the project using php

--- header.php
+++ header.php
@@ -6,6 +6,19 @@
     <title>La Pizzeria</title>
     <?php wp_head(); ?>
 </head>
+
 <h1>Welcome to flyhightech</h1>
 
 <body>
+
+    //Below is where the image is located.
+
+    <header class="site-header">
+
+        <div class="logo">
+
+            <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg">
+
+        </div>
+
+    </header>
