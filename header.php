4bbf075529b4e7b0c02e2ef8007a99b03172910e
Bernard Huff <bernard.huff008@gmail.com>
2018-07-14 00:13:45 -0500

    Sweep

--- header.php
+++ header.php
@@ -17,7 +17,11 @@
 
         <div class="logo">
 
-            <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg">
+            <a href="<?php echo esc_url( home_url('/') ); ?>">
+
+                <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg"     class="logoimage">
+
+            </a>
 
         </div>
 
