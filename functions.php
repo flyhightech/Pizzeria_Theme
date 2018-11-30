dcd518b7680d7f639eba36e0aa691c8f3a57c2e8
Bernard Huff <bernard.huff008@gmail.com>
2018-11-30 12:48:17 -0600

    sweep

--- functions.php
+++ functions.php
@@ -5,11 +5,13 @@ function lapizzeria_styles() {
     // Adding stylesheets
 
     wp_register_style('normalize', get_template_directory_uri() . 'css/normalize.css', array(), '8.0.0');
+    wp_register_style('fontawesome', get_template_directory_uri() . 'css/font-awesome.css', array(), '4.7.0');
     wp_register_style('style', get_template_directory_uri() . '/style.css', array(), '1.0');
 
     // Enqueue the style
 
     wp_enqueue_style('normalize');
+    wp_enqueue_style('fontawesome');
     wp_enqueue_style('style');
 
 }
