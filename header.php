6f8c3dcd3379cb6f58d9ed92e89c4d8f6443e147
Bernard Huff <bernard.huff008@gmail.com>
2019-04-17 00:53:31 -0500

    Sweep

--- header.php
+++ header.php
@@ -14,17 +14,11 @@
     <!--    Below is where the image is located.   -->
 
     <header class="site-header">
-
         <div class="container">
-
             <div class="logo">
-
                 <a href="#<?php echo esc_url(home_url('/')); ?>">
-
-                <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg" class="logoimage">
-
-            </a>
-
+                    <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg" class="logoimage">
+                </a>
             </div>
 
             <!-- End of the Logo -->
@@ -38,32 +32,28 @@
                         'container_id'     => 'socials',
                         'link_before'      => '<span class="sr-text">',
                         'link_after'       => '</span>',
-                        );
-                     wp_nav_menu($args);
-                ?>
+                    );
+                    wp_nav_menu($args);
+                    ?>
                 </div>
-
                 <!--         This is where the social links code ends!           -->
-
                 <div class="address">
                     <p>123 Seseme street, Chicago Il 60292</p>
                     <p>999-298-2999</p>
                 </div>
-
                 <!--       Address and the phone number section ends here        -->
-
             </div>
         </div>
     </header>
     <div class="main-menu">
         <div class="navigation">
-            <?php 
-                $args = array(
-                    'theme_location'  => 'header-menu',
-                    'container'       => 'nav',
-                    'container_class' => 'site-nav'
-                );
-                wp_nav_menu($args);
+            <?php
+            $args = array(
+                'theme_location'  => 'header-menu',
+                'container'       => 'nav',
+                'container_class' => 'site-nav'
+            );
+            wp_nav_menu($args);
             ?>
 
         </div>
