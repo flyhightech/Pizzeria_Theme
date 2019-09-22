ef1eb6bfeed829cba54a6192ef3c5702cfb8bea9
Bernard Huff <bernard.huff008@gmail.com>
2019-09-22 04:31:29 -0500

    Sweep

---
+++ page-about-us.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+    <?php while(have_posts(  )) : the_post(); ?>
+
+        <div class="hero" style="background-image:url(<?php echo get_the_post_thumbnail_url(); ?>); ">
+            <div class="hero-content">
+                <div class="hero-text">
+                    <h2><?php the_title(); ?></h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="main-content container">
+            <main class="text-center content-text">
+                <?php the_content(); ?>
+            </main>
+        </div>
+
+        <div class="box-information container clear">
+            <div class="box">
+                <div class="content-box">
+                    
+                </div>
+            </div>
+        </div>
+
+    <?php endwhile; ?>
+
+<?php get_footer(); ?>
